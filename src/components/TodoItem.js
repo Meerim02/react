e8392116cf1d46ab0748e8286cf1a50b53fc975f
Your Name <you@example.com>
2021-02-10 19:04:04 +0600

    state

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -1,13 +1,35 @@
-import React from 'react';
-
-const TodoItem = (props) => {
-    let name = "list-group-item " 
-    if (props.important){
-    name +="active"
+import React, { Component } from 'react';
+class TodoItem extends Component {
+    state = {
+        important: false,
+        done: false
     }
-    return (
-        <li className={name}>{props.children}</li>
-    );
-};
+    updateState = (state) => {
+        return {
+            important: !state.important
+        }
+    }
+    postUpdate = () => {
+        console.log(this.state.important);
+    }
+    onClick = () => {
+        this.setState(this.updateState, this.postUpdate)
+    }
+    //  onClick = () =>{
+    //      this.setState({
+    //          important:!this.state.important
+    //      })
+    //  }
+    render() {
+        const className = this.state.important ? "list-group-item todo-item active" : "todo-item list-group-item"
+        const spanClassName = 'text done'
+        return (
+            <li  className={className}>
+                <span onClick={this.onClick} className={spanClassName}> {this.props.children}</span>
+                <span className ="btn btn-success">Done</span>
+            </li>
+        );
+    };
+}
 
 export default TodoItem;
--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,10 +1,10 @@
-import React from 'react';
+import React  from 'react';
 import TodoItem from './TodoItem';
 
 const TodoList = () => {
     return (
-       <ul className="list-group mt-3">
-           <TodoItem important>Cook</TodoItem>
+       <ul className="list-group mt-2">
+           <TodoItem >Cook</TodoItem>
            <TodoItem>Read book</TodoItem>   
            <TodoItem>Sleep</TodoItem>
 
