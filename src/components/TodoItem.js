00d7950d4137f1a753e01820ecf4a19a5e93b455
Your Name <you@example.com>
2021-02-15 18:05:12 +0600

    done done

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -4,29 +4,28 @@ class TodoItem extends Component {
         important: false,
         done: false
     }
-    updateState = (state) => {
-        return {
-            important: !state.important
-        }
-    }
-    postUpdate = () => {
-        console.log(this.state.important);
-    }
     onClick = () => {
-        this.setState(this.updateState, this.postUpdate)
+        this.setState((state) => {
+            return {
+                important: !state.important
+            }
+        })
     }
-    //  onClick = () =>{
-    //      this.setState({
-    //          important:!this.state.important
-    //      })
-    //  }
+    onClickDone = () => {
+        this.setState((state) => {
+            return {
+                done: !state.done
+            }
+        })
+    }
     render() {
         const className = this.state.important ? "list-group-item todo-item active" : "todo-item list-group-item"
-        const spanClassName = 'text done'
+        const spanClassName= this.state.done ? "text done" : "text"
         return (
-            <li  className={className}>
+            <li className={className}>
                 <span onClick={this.onClick} className={spanClassName}> {this.props.children}</span>
-                <span className ="btn btn-success">Done</span>
+                <button onClick={this.onClickDone} className="btn btn-success">Done</button>
+
             </li>
         );
     };
