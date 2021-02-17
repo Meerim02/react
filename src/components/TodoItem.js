73d9ab73a2c0f6b0ee5f03c6c3da20873e553e32
Your Name <you@example.com>
2021-02-17 17:47:34 +0600

    changeImportant

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -1,34 +1,17 @@
 import React, { Component } from 'react';
 class TodoItem extends Component {
-    state = {
-        important: false,
-        done: false
-    }
-    onClick = () => {
-        this.setState((state) => {
-            return {
-                important: !state.important
-            }
-        })
-    }
-    onClickDone = () => {
-        this.setState((state) => {
-            return {
-                done: !state.done
-            }
-        })
-    }
     render() {
-        const className = this.state.important ? "list-group-item todo-item active" : "todo-item list-group-item"
-        const spanClassName= this.state.done ? "text done" : "text"
+        let className = this.props.important ? "list-group-item todo-item active" : "todo-item list-group-item"
+        const spanClassName = this.props.done ? "text done" : "text"
         return (
             <li className={className}>
-                <span onClick={this.onClick} className={spanClassName}> {this.props.children}</span>
+                <span onClick={this.props.changeInportant} className={spanClassName}> {this.props.children}</span>
                 <button onClick={this.onClickDone} className="btn btn-success">Done</button>
 
             </li>
-        );
-    };
+        )
+    }
 }
 
 export default TodoItem;
+
--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,15 +1,37 @@
-import React  from 'react';
+import React, { Component }  from 'react';
 import TodoItem from './TodoItem';
 
-const TodoList = () => {
-    return (
-       <ul className="list-group mt-2">
-           <TodoItem >Cook</TodoItem>
-           <TodoItem>Read book</TodoItem>   
-           <TodoItem>Sleep</TodoItem>
 
-         </ul>
-    );
-};
+class TodoList extends Component {
+    state = {
+        todoList:[
+            {name:"Read Book2",important:true},
+            {name:"Cook2",important:false},
+            {name:"Sleep2",important:true},
+            {nme:"Play2",important:false}
+        ]
+    }
+
+
+    changeInportant = (name) => () => {
+        console.log('this change'+name);
+    }
+
+
+    render(){
+        return (
+            <ul className="list-group mt-2">
+                {this.state.todoList.map((todo)=>{
+                    return (<TodoItem changeInportant={this.changeInportant(todo.name)} important={todo.important}>
+                        {todo.name}
+                        </TodoItem>)
+                })}
+              </ul>
+         );
+     }
+    
+ };
+
+   
 
 export default TodoList;
